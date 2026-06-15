790baca71089c916fa62522d0ffed6a851d0d4be
inmisin <omeroflu2@gmail.com>
2026-06-16 01:31:38 +0300

    im tryin

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 
 struct Vector
 {
@@ -9,40 +11,64 @@ struct Vector
 
 struct OfluFile
 {
-    char ext[4];
+    int id;
     struct Vector position;
 };
 
 
 void CreateFile()
 {
-    char text[] = "napan be";
+    struct OfluFile f1;
+    struct Vector p1;
+    p1.x = 1.5;
+    p1.y = 2.5;
+    p1.z = 3.5;
+    f1.id = 1;
+    f1.position = p1;
+
+    struct OfluFile f2;
+    struct Vector p2;
+    p2.x = 11;
+    p2.y = 42.5;
+    p2.z = 3.1;
+    f2.id = 2;
+    f2.position = p2;
+
+    struct OfluFile f3;
+    struct Vector p3;
+    p3.x = 5.5;
+    p3.y = 1.5;
+    p3.z = 3.5;
+    f3.id = 3;
+    f3.position = p3;
+
     FILE *file = fopen("test.oflu", "w");
 
-    int len = sizeof(char) / sizeof(text[0]);
+    fwrite(&f1, sizeof(struct OfluFile), 1, file);
+    fwrite(&f2, sizeof(struct OfluFile), 1, file);
+    fwrite(&f3, sizeof(struct OfluFile), 1, file);
 
-    fwrite(text, sizeof(char), len, file);
+
     fclose(file);
 }
 
 void ReadFile()
 {
-    int buffer[5];
+    struct OfluFile f;
     FILE *file = fopen("test.oflu", "r");
 
-    fread(buffer, sizeof(int), 5, file);
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Element %d: %d \n", i+1, buffer[i]);
-    }
+    fread(&f, sizeof(struct OfluFile), 2, file);
     
+    printf("What's written in the file is: %d \n" , f.id);
+
     fclose(file);
 }
 
 int main()
 {
-    CreateFile();
+    // tek tek kullan yoksa 
+    // bellekten bilgileri aldığı için doğru çalışıp çalışmadığı belli olmuyor
+    //CreateFile();
     ReadFile();
     return 0;
 }
